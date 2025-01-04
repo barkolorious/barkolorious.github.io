b6398c1b774bc70e67a9827d105c63bd47e9ab10
barkolorious <barkin.ozsoy@gmail.com>
2025-01-04 22:41:18 +0300

    Dynamic Page Loading

--- script.js
+++ script.js
@@ -1,3 +1,259 @@
+const projectsData = {
+  "projects": [
+    {
+      "id": "borealis",
+      "title": "borealis Logo",
+      "tags": ["Logo"],
+      "description": "Logo for borealis",
+      "statement": [
+        "My friend borealis aka Elif Bengisu Gezder didn't have any profile picture so I wanted to design a logo for her to use. Her username is coronaborealis or borealis for short and she is interested in music so I tried to combine these two. I called it scorealis.",
+        "I used the music symbols and the constellation. The duration of notes approximately represents the size of the stars.",
+        "She liked the design so she used it. However, now she doesn't use it bu I don't know why."
+      ],
+      "captions": [
+        "Final design",
+        "First attempt",
+        "With text",
+        "With score lines",
+        "Inspiration"
+      ]
+    }, {
+      "id": "airqm",
+      "title": "AirQ-M Logo",
+      "tags": ["Logo"],
+      "description": "Logo for my project \"AirQ-M: Air Quality Meter\"",
+      "statement": [
+        "Logo for my project \"AirQ-M: Air Quality Meter\""
+      ],
+      "captions": [
+        "Final design",
+        "First sketch",
+        "Other attempt"
+      ]
+    }, {
+      "id": "das_coffeetal",
+      "title": "Das Coffeetal Logo",
+      "tags": ["Logo"],
+      "description": "Logo for an imaginary concept coffee shop \"Das Coffeetal\".",
+      "statement": [
+        "One day when I was talking to my brother I had an amazing idea. Nowadays, there are so many concept coffee shops and why not open a one with a communism concept, maybe you could open it in METU.",
+        "If you haven't figured it out yet, the name is a word play on \"Das Kapital\". I think it is funny.",
+        "In the first sketch I wanted the logo to be a coffee cup. But my brother suggested it to be a coffee pot. I think it's better. You can see the sickle as a part of the handle with the spoon standing for the hammer.",
+        "The font is \"Bourgeois\" the font of METU and it looks similar to Cyrillic alphabet."
+      ],
+      "captions": [
+        "Final design",
+        "First sketch"
+      ]
+    }, {
+      "id": "liselerde_bilim_uygulamalari",
+      "title": "LBU Logo Redisgn",
+      "tags": ["Logo", "Redesign"],
+      "description": "Redesigning the LBU logo.",
+      "statement": [
+        "When I participated in LBU, I hated the logo. I think it looks disgusting. So, I tried to design my own version.",
+        "I made 2 designs. I think Design1 is better however I don't know it is really usable in the event. The second design is much more usable but very very boring.",
+        "But in my opinion, both designs are much much better than the current one."
+      ],
+      "captions": [
+        "Design1",
+        "Design2",
+        "Current design",
+        "Different font for Design1",
+        "Different color scheme for Design1",
+        "Different color scheme for Design1",
+        "Different color schemes for Design2"
+      ]
+    }, {
+      "id": "fource",
+      "title": "Fource Logo",
+      "tags": ["Logo"],
+      "description": "Logo design for my project \"Fource: Fourier based Complex Encryption\"",
+      "statement": [
+        "I have an encryption project based on Discrete Fourier Transform and linear algebra.",
+        "I wanted it to have a logo, and the logo to be elegant. So I chose a serif font.",
+        "The background design is a Fourier transformed version of toothsaw function.",
+        "For more info on my project check out the <a href=\"https://github.com/barkolorious/fource\" class=\"back-link\">github repo</a>."
+      ],
+      "captions": [
+        "Final design",
+        "Different color schemes"
+      ]
+    }, {
+      "id": "aerop",
+      "title": "AEROP Logo",
+      "tags": ["Logo"],
+      "description": "Logo for yavuzskarahan's project \"AEROP\".",
+      "statement": [
+        "My friend yavuzskarahan aka Yavuz Selim Karahan, has a project called AEROP which is basically an \"drone design optimization\" program. It does have a logo but I wanted to give it a try.",
+        "When I made this logo I've just started to listen IDKHOW and I loved their album covers. I tried to replicate the vibe. So I stole the colors of the album \"1981 Extended Play\".",
+        "My first attempts looked like MasterCard so I tried changing them and made the propeller design. It can be improved like making it better for monochrome? I don't know. I'll keep working on it."
+      ],
+      "captions": [
+        "Final design",
+        "Different color schemes",
+        "Different orientations",
+        "First attempt",
+        "Different orientations and colors",
+        "A mock-up",
+        "Another mock-up"
+      ]
+    }, {
+      "id": "opaque",
+      "title": "opaque Logo",
+      "tags": ["Logo"],
+      "description": "Logo design for my project \"opaque: Open-source Portable Air Quality Evaluator\".",
+      "statement": [
+        "Logo design for my project \"opaque: Open-source Portable Air Quality Evaluator\""
+      ],
+      "captions": [
+        "Final design",
+        "First attempt",
+        "First sketches",
+        "Changed the 'a'",
+        "Thinner version",
+        "Rounder and bigger",
+        "Squarer"
+      ]
+    }, {
+      "id": "barkolorious",
+      "title": "barkolorious Logo",
+      "tags": ["Logo"],
+      "description": "My logo which represents a blackhole",
+      "statement": [
+        "Logo for myself. I wanted a blackhole logo which was inspired of Kurzgesagt's blackhole. However, there are endless blackhole logos in the internet. So I wanted mein to stand out. So I tried several design but when I was poking around with Inkscape's perspective tool, I accidentaly made the last design. And it looked just right.",
+        "I tried some other color schemes but blue gradient was the best. I tried to add a border, I used it liked that for some time but I changed it."
+      ],
+      "captions": [
+        "Final design",
+        "Different color scheme",
+        "First sketches",
+        "First sketches",
+        "First sketches",
+        "Last design with border",
+        "Inspiration"
+      ]
+    }, {
+      "id": "believers_never_die",
+      "title": "Be(lie)vers Never Die Playlist Cover",
+      "tags": ["Playlist Cover"],
+      "description": "Playlist cover for borealis",
+      "statement": [
+        "Playlist cover for borealis aka Elif Bengisu Gezder. The font is \"Kurs\" which is the font used in Singapore World Chess Championship 2024. When I saw this font I knew I had to use it somewhere and this was the right place to use it.",
+        "In my first attempt I used a bad color scheme which I change to monochrome black and red. And the stripe in the first attemp wasn't good, it looked out of place but in the final version I think it fits right in."
+      ],
+      "captions": [
+        "Final design",
+        "Fir"
+      ]
+    }
+  ]
+};
+
+function loadContent() {
+  try {
+    const hash = window.location.hash;
+    const mainContent = document.querySelector('main');
+
+    if (hash.startsWith('#project/')) {
+      const projectId = hash.replace('#project/', '');
+      const project = projectsData.projects.find(p => p.id === projectId);
+
+      console.log(project);
+      
+      if (project) {
+        mainContent.innerHTML = generateProjectPage(project);
+      } else {
+        mainContent.innerHTML = '<p class="error">Project not found</p>';
+      }
+    } else {
+      mainContent.className = 'portfolio-grid';
+      mainContent.innerHTML = '';
+      
+      projectsData.projects.forEach(project => {
+        const portfolioItem = createPortfolioItem(project);
+        mainContent.appendChild(portfolioItem);
+      });
+    }
+  } catch (error) {
+    console.error('Error loading content:', error);
+    const mainContent = document.querySelector('main');
+    mainContent.innerHTML = '<p class="error">Failed to load content. Please try again later.</p>';
+  }
+}
+
+function createPortfolioItem(project) {
+  const portfolioItem = document.createElement('a');
+  portfolioItem.href = '#project/' + project.id; // Changed to use hash routing
+  portfolioItem.className = 'portfolio-item';
+
+  portfolioItem.innerHTML = `
+    <div class="image-container">
+      <img src="assets/${project.id}/img00.png" alt="${project.title}">
+    </div>
+    <div class="item-content">
+      <h2>${project.title}</h2>
+      <div class="tags">
+        ${project.tags.map(tag => `<span class="tag">${tag}</span>`).join('')}
+      </div>
+      <p>${project.description}</p>
+    </div>
+  `;
+
+  return portfolioItem;
+}
+
+function generateProjectPage(project) {
+  document.title = `${project.title} - Portfolio`;
+  
+  const content = `
+    <div class="project-container">
+      <div>
+        <a href="#" class="back-link">← Back to Portfolio</a>
+        <div class="image-grid">
+          ${project.captions.map((caption, index) => `
+            <div class="image-wrapper">
+              <img 
+                src="assets/${project.id}/img${String(index).padStart(2, '0')}.png" 
+                alt="${caption}"
+                onclick="openModal(this.src, 'image', this.nextElementSibling.textContent)"
+              >
+              <p class="image-caption"><center>Figure ${index + 1}: ${caption}</center></p>
+            </div>
+          `).join('')}
+        </div>
+      </div>
+
+      <div class="project-info">
+        <h2>${project.title}</h2>
+        <div class="tags">
+          ${project.tags.map(tag => `<span class="tag">${tag}</span>`).join('')}
+        </div>
+        ${project.statement.map(paragraph => `<p>${paragraph}</p>`).join('')}
+      </div>
+    </div>
+
+    <div class="modal" id="imageModal">
+      <button class="close-modal" onclick="closeModal()">×</button>
+      <div id="modalContent"></div>
+      <p id="modalCaption" class="image-caption"></p>
+    </div>
+  `;
+  
+  return content;
+}
+
+document.addEventListener('DOMContentLoaded', loadContent);
+window.addEventListener('hashchange', loadContent);
+
+document.addEventListener('error', function(e) {
+  if (e.target.tagName.toLowerCase() === 'img') {
+    e.target.src = 'assets/placeholder.png';
+  }
+}, true);
+
+// -----------------------------------------------
+
 function openModal(content, type, caption = '') {
   const modal = document.getElementById('imageModal');
   const modalContent = document.getElementById('modalContent');
